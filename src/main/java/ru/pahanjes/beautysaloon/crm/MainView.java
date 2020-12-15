f7d7663a3ce5d2c5b8487d8139c179f0dd4730e7
Pahanjes <pahanjes.spb@gmail.com>
2020-12-15 13:56:32 +0300

    Initial commit

---
+++ src/main/java/ru/pahanjes/beautysaloon/crm/MainView.java
@@ -0,0 +1,12 @@
+package ru.pahanjes.beautysaloon.crm;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+}
